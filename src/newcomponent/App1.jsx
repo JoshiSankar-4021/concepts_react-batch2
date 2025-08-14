d1d28d3a574867f3b00b458590a966eb549c4c57
JoshiSankar-4021 <joshisankar5051@gmail.com>
2025-08-14 15:13:44 +0530

    function and component creations

---
+++ src/newcomponent/App1.jsx
@@ -0,0 +1,10 @@
+import './App1.css';
+
+function App1(){
+    return(
+        <div>
+            <h1>REACT</h1>
+        </div>
+    );
+}
+export default App1;
---
+++ src/newcomponent1/App2.jsx
@@ -0,0 +1,11 @@
+import './App2.css';
+
+function App2(){
+    return(
+        <div>
+            <h1>Newcomponent 2</h1>
+        </div>
+    );
+}
+
+export default App2;
